fae48fa774868a1f5a57ac1a5bd9a11a8299e9ab
Shreyas Gaonkar <shreyas.gaonkar@yahoo.com>
2020-02-26 23:01:33 +0000

    Add landing page

---
+++ scripts/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.css' // Import precompiled Bootstrap css
+import '@fortawesome/fontawesome-free/css/all.css'
+import '../styles/main.scss';
+
+document.querySelector('h1').textContent = bro(`How's it going`);
