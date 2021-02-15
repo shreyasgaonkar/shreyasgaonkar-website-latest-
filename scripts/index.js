d619343d94795d991cdeae97f43b5096ce816369
shreyasgaonkar <shreyas.gaonkar@yahoo.com>
2021-02-15 19:30:07 +0000

    Content Updates
    - Updated background images and CSS to use smaller res images for
    fast content loading
    - Updated exprerience, projects and dataset filename.
    - Removed unwanted comments/code from previous commits
    - Fixed URLs on footer and header section

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,3 @@
-// import 'bootstrap';
-// import 'bootstrap/dist/css/bootstrap.css';
-// import '@fortawesome/fontawesome-free/css/all.css';
-
-// url = "https://bspd5cmmd4.execute-api.us-west-2.amazonaws.com/v1?stopid=110"
-// var fetchData = $.getJSON(url, gotData, "jsonp");
 
 function addExperience(k, val) {
     const company = val['company'];
@@ -15,7 +9,6 @@ function addExperience(k, val) {
 }
 
 function addProjects(k, val) {
-    // console.log(k, val);
     const project = val['name'];
     const skills = val['skills'];
     const href = val['href'];
@@ -24,8 +17,6 @@ function addProjects(k, val) {
 }
 
 function addTestimonials(k, val) {
-    console.log(k, val);
-
     const name = val['name'];
     const title = val['title'];
     const company = val['company'];
@@ -35,29 +26,13 @@ function addTestimonials(k, val) {
     return ('<div class="box"><img src="' + img + '" alt=""><blockquote>' + content + '<span>' + name + ',<span class="job-dates"> ' + title + ' at ' + company + '</span></span></blockquote></div>');
 }
 
-// $.ajax({
-//     url: "https://gist.githubusercontent.com/shreyasgaonkar/080abcf2379da20764f66710dfe214fc/raw/249797f279da4c0c7ce76ba631134e254f9337dd/sample.json",
-//     success: function(result) {
-//         var data = jQuery.parseJSON(result);
-//         data = data.person;
-//         $.each(data, function(key, val) {
-//
-//             $('#experience').append(addExperience(key, val))
-//         });
-//     },
-//     failure: function(result) {
-//         console.log(`Result is ${result}`);
-//     },
-//     async: false
-// });
-
 $.getJSON("../data/experience.json", function(data) {
     $.each(data, function(key, val) {
         $('#experience').append(addExperience(key, val));
     });
 }, "jsonp");
 
-$.getJSON("../data/data.json", function(data) {
+$.getJSON("../data/projects.json", function(data) {
     $.each(data, function(key, val) {
         $('#projects').append(addProjects(key, val))
     });
@@ -68,19 +43,3 @@ $.getJSON("../data/testimonals.json", function(data) {
         $('#testimonials').append(addTestimonials(key, val))
     });
 }, "jsonp");
-
-// $.ajax({
-//     url: "data.json",
-//     success: function(result) {
-//         var data = jQuery.parseJSON(result);
-//         data = data.projects;
-//         $.each(data, function(key, val) {
-//
-//             $('#projects').append(addProjects(key, val))
-//         });
-//     },
-//     failure: function(result) {
-//         console.log(`Result is ${result}`);
-//     },
-//     async: false
-// });
